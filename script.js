2c6de73b6bde669f68646e2aa95071b455bbb7c7
camille breillot <camille.breillot@gmail.com>
2022-01-17 15:34:42 +0100

    function displayAnswer working

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ arrayQuestions = Array.from(questions);
 answers = document.querySelectorAll('.answer-details');
 arrayAnswers = Array.from(answers);
 
-//write function
+//write functions
+
+const displayAnswer = (event) => {
+  answer = event.currentTarget.nextElementSibling.children[0];
+  answer.style.display == "none" || answer.style.display == '' ? answer.style.display = "block" : answer.style.display = "none";
+
+  questionTitle = event.currentTarget.children[0];
+  questionTitle.classList.toggle('title-active');
+
+  arrow = event.currentTarget.children[1].children[0];
+  arrow.classList.toggle('arrow-active');
+};
 
 //listen to event
+arrayClickableQuestions.forEach(clickableQuestion => {
+  clickableQuestion.addEventListener('click', displayAnswer);
+});
