b2bd5c1a8982b3ce70ff20ff7af3812e82a4507c
camille breillot <camille.breillot@gmail.com>
2022-01-17 14:53:09 +0100

    js file created and elements selected

---
+++ script.js
@@ -0,0 +1,16 @@
+// select all elements needed
+clickableQuestions = document.querySelectorAll('.question');
+arrayClickableQuestions = Array.from(clickableQuestions);
+
+arrows = document.getElementsByClassName('arrow');
+arrayArrows = Array.from(arrows);
+
+questions = document.querySelectorAll('.question h4');
+arrayQuestions = Array.from(questions);
+
+answers = document.querySelectorAll('.answer-details');
+arrayAnswers = Array.from(answers);
+
+//write function
+
+//listen to event
